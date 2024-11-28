54e97a5050c847a1220f534433eb9a1b1c00d1f6
nicole orjuela <nicoleorjuela1552@gmail.com>
2024-11-28 10:53:20 -0500

    sesion

--- src/componentes/barras/BarraCliente.js
+++ src/componentes/barras/BarraCliente.js
@@ -4,6 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faBox, faCalendar, faShoppingBasket, faCalendarCheck, faConciergeBell, faUser } from '@fortawesome/free-solid-svg-icons';
 import Swal from 'sweetalert2';
 import axios from 'axios';
+import API_URL from '../../config/config'; 
+
  
  
 const BarraCliente = () => {
@@ -44,7 +46,7 @@ const BarraCliente = () => {
  
   const handleLogout = async () => {
     try {
-      const response = await axios.post('http://localhost:8000/usuarios/cerrarsesion', {}, { withCredentials: true });
+      const response = await axios.post(`${API_URL}/usuarios/cerrarsesion`, {}, { withCredentials: true });
       if (response.status === 200) {
             // Eliminar el rol del usuario del almacenamiento local
             localStorage.removeItem('rol');
